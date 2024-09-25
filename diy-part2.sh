779127d5d7f83d0238758d701c530bac1b1ac094
huntersonzjg <78995644+huntersonzjg@users.noreply.github.com>
2024-09-25 11:10:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.65.73/g' package/base-files/files/bin/config_generate
+
+# Modify default password
+sed -i 's#root:::0:99999:7:::#root:$1$jFne/Yvy$T6jwwrT53j.uX7sNsu4.t0:19789:0:99999:7::: #g'  package/base-files/files/etc/shadow
